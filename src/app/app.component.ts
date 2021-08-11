fabdb4cf94b9bcb5821028a33c546c7f6356d915
RugaCoder <fruganyumisa@gmail.com>
2021-08-11 12:55:00 +0300

    finished testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
+import { KeyValuePipe } from '@angular/common';
+import { KeyValue } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { MapType } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { filter, tap } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
@@ -10,6 +14,8 @@ import { Observable } from 'rxjs';
 export class AppComponent implements OnInit{
 
   data: Observable<any> = new Observable();
+  signature: any
+  photo: any
 
   constructor(
     public http: HttpClient
@@ -20,8 +26,25 @@ export class AppComponent implements OnInit{
     return this.http.get(file);
   }
 
+  convertDataUrl(dataUrl: any) {
+    
+    return "data:image/png;base64,"+dataUrl
+}
+
   ngOnInit(): void {
-    this.data = this.getSampleData();
+    this.data = this.getSampleData().pipe(
+      tap((data: any) => {
+        this.photo = data["PHOTO"];
+        this.photo = this.convertDataUrl(this.photo);
+        this.signature = data["SIGNATURE"];
+        this.signature = this.convertDataUrl(this.signature);
+
+        delete data["PHOTO"];
+        delete data["SIGNATURE"];
+
+      }),
+    );
+      
   }
 
 }
